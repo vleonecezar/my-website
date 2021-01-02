b7312495c1b9382495dabe37c376ddadc34c87f7
Vitor Leone Cezar <vleone.social@gmail.com>
2021-01-02 02:55:52 -0300

    some styles. make the footer and a home component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>
+        <header>
         <div className="container">
             <a id ="header-logo" href="#">VitorCezar</a>
-                <nav>
-                    <ul id="nav-items">
+                <nav id="header-navbar">
+                    <ul>
                         <li>
                             <a href="#">sobre</a>
                         </li>
@@ -21,6 +21,24 @@ function App() {
                 </nav>
         </div>
     </header>
+        <footer>
+            <div className="container">
+                <nav id="footer-navbar">
+                    <ul id="nav-items">
+                        <li>
+                            <a href="#">sobre</a>
+                        </li>
+                        <li>
+                            <a href="#">portfólio</a>
+                        </li>
+                        <li>
+                            <a href="#">contato</a>
+                        </li>
+                    </ul>
+                </nav>
+                <p>© 2021 Vitor Cezar</p>
+            </div>
+        </footer>
     </div>
   );
 }
