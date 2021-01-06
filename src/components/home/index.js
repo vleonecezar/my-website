c4c42514de92509ced7bc8f8fc5ce590318f801b
Vitor Leone Cezar <vleone.social@gmail.com>
2021-01-06 07:45:31 -0300

    some styles and picture

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,6 +2,27 @@ import React, { useState, useEffect } from 'react';
 import './styles.css'
 import vitorcezar_image from '../../assets/images/vitorcezar_image.jpg'
 
+// Projects Mouse Effects
+
+function projectsMouseOver(id) {
+    const elements = document.querySelectorAll(".project")
+    
+    elements.forEach(element => {
+        element.style.opacity = "0.4"
+    })
+
+    document.getElementById(id).style.opacity = "1"
+}
+
+function projectsMouseOut() {
+    const elements = document.querySelectorAll(".project")
+    elements.forEach(element => {
+        element.style.opacity = "1"
+    })
+}
+
+// -----------------------------
+
 async function getProjects() {
     try {
         const response = await fetch('http://localhost:3000/api/projects.json')
@@ -11,7 +32,7 @@ async function getProjects() {
         console.error(error)
     }
 }
-
+    
 const Home = () => {
 
     const [homeContent, setHomeContent] = useState([])
@@ -19,16 +40,17 @@ const Home = () => {
     useEffect(() => {
         getProjects().then(data => {
             setHomeContent(
-                data['projects'].map(project => (
-                    <div className="project">
-                        <img key={project.id} src={project.image} alt={`foto do projeto ${project.name}`}/>
-                        <h3>{project.name}</h3>
-                        <p>{project.description}</p>
-                    </div>
-                ))
+             data['projects'].map(project => (
+                <div id={project.id} className="project" onMouseOver={() => projectsMouseOver(project.id)} onMouseOut={() => projectsMouseOut()}>
+                    <img key={project.id} src={project.image} alt={`foto do projeto ${project.name}`}/>
+                    <h3>{project.name}</h3>
+                    <p>{project.description}</p>
+                </div>
+
+            ))
             )
         })
-    }, [])
+    })
 
     return (
         <section>
@@ -36,15 +58,25 @@ const Home = () => {
                 <div className="profile">
                     <img src={vitorcezar_image} alt="Foto de Vitor Cezar"/>
                     <div className="profile-info">
-                        <h2>{'< Desenvolvedor >'}</h2>
+                        <h2>{'< Web Developer >'}</h2>
                         <p>
                         Desenvolvedor Front End em constante aprendizado. Focado em Javascript, 
                         buscando sempre o codigo limpo e eficiente.
                         </p>
+                        <div className="profile-buttons">
+                            <a href="http://localhost:3000">
+                                <i class="fab fa-github"></i>
+                                Github
+                            </a>
+                            <a href="#">
+                                <i class="fab fa-instagram"></i>
+                                Instagram
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-            <h2 id="projects-title">Ultimos Projetos</h2>
+            <h2 id="projects-title">Meus Ultimos Projetos</h2>
             <div className="projects-container">
                 {homeContent}
             </div>
